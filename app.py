3dc0e08c400d59c7a59fe9117bfec72ddbc9a10c
dishavanani13 <dishvanani13@gmail.com>
2026-04-05 09:06:48 -0400

    User Conversation Retention

--- app.py
+++ app.py
@@ -4,23 +4,26 @@ from openai import OpenAI
 
 client = OpenAI( api_key=config.API_KEY)
 app = Flask(__name__)
+conversation_history = []
 @app.route("/")
 def index():
     return render_template("index.html")
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
+    model_engine = "gpt-3.5-turbo"
+    conversation_history.append({'role': 'user', 'content': userText})
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": f"{userText}"},
-        ],
+        
+        model=model_engine,
+        messages= conversation_history,
+
         max_tokens=1024,
         n=1,
         stop=None,
         temperature=1,
     )
-    answer = response.choices[0].message.content
-    return str(answer)
+    ai_responce = response.choices[0].message.content
+    return str(ai_responce)
 if __name__ == "__main__":
     app.run()
