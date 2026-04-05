edcb1ac867b60abd50872b0be4757d2051b1f6bd
dishavanani13 <dishvanani13@gmail.com>
2026-04-05 08:08:34 -0400

    fetching data from chatGPT API

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import config
+from openai import OpenAI
 
 client = OpenAI( api_key=config.API_KEY)
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Hello, World!"
-
+    return render_template("index.html")
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": f"{userText}"},
+        ],
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=1,
+    )
+    answer = response.choices[0].message.content
+    return str(answer)
 if __name__ == "__main__":
     app.run()
